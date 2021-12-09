826239fbba8d20a9723926a5e0b3358ca3ec4b37
Grustentier <29798768+Grustentier@users.noreply.github.com>
2021-12-09 16:10:39 +0100

    Add files via upload

--- modules/faceMorphing/morphFaces.py
+++ modules/faceMorphing/morphFaces.py
@@ -1,12 +1,10 @@
 '''
 Created on 02.09.2021
 
-@author: grustentier
+@author: Grustentier
 '''
 
 print("""
-
-
   ___                       ___                                 __  __                      _                
  | __|  __ _   __   ___    |_ _|  _ __    __ _   __ _   ___    |  \/  |  ___   _ _   _ __  | |_    ___   _ _ 
  | _|  / _` | / _| / -_)    | |  | '  \  / _` | / _` | / -_)   | |\/| | / _ \ | '_| | '_ \ | ' \  / -_) | '_|
@@ -28,7 +26,8 @@ source: https://pypi.org/project/facemorpher/
 
 __version__ = '0.1'
 
-import os 
+import os
+import sys 
 import argparse
 import facemorpher
 
@@ -38,7 +37,8 @@ parser.add_argument('--export_dir', default=None, type=str, help='The export/out
 parser.add_argument('--display', default=False, type=str, help='True or 1 for displaying morphing results in separate popups.')   
 arguments = parser.parse_args()
 
-FILE_TYPES = ["bmp","jpg","jpeg","png"]
+FILE_TYPES = ["bmp", "jpg", "jpeg", "png"]
+
 
 def boolean_string(s):
     print(str(s).lower())
@@ -46,50 +46,61 @@ def boolean_string(s):
         raise ValueError('Not a valid boolean string')
     return str(s).lower() == 'true' or str(s).lower() == '1'
 
-def collectImageFilePaths():
+
+def collectImageFilePaths(input_dir):
     filePaths = []
-    for root, _, files in os.walk(arguments.input_dir):  
-        if root.endswith(os.sep) is False:root+=os.sep
+    for root, _, files in os.walk(input_dir): 
+        if root.endswith(os.sep) is False:root += os.sep
         filePaths.extend([root + file for file in files if str(file).split(".")[-1].lower() in FILE_TYPES])
     return filePaths
+
 
 def createExportDir(exportDirPath):
     if os.path.exists(exportDirPath) == False:
         print("Create Directory: " + str(exportDirPath))    
         os.makedirs(exportDirPath, 755)
+
         
-def printProgress(steps,maximum):
-    output = ""
-    maxSteps2Console = 20
-    for _ in range(0,int((steps/maximum)*maxSteps2Console)):
-        output +="."
-    print("["+output+"]", str(int(round((steps/maximum)*100,0)))+"%")         
+def printProgress(steps, maximum, name="progress", bar_length=20, width=20): 
+    if maximum == 0:
+        percent = 1.0
+    else:
+        percent = float(steps) / maximum
+    arrow = '-' * int(round(percent * bar_length) - 1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+    # sys.stdout.write("\r{0: <{1}} : [{2}]{3}%".format(name, width, arrow + spaces, int(round(percent*100))))
+    sys.stdout.write("\r{0: <{1}}[{2}]{3}%".format("", 0, arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()    
+    
+    if steps >= maximum: 
+        sys.stdout.write('\n\n')         
 
 
-if __name__ == "__main__":
-    assert arguments.input_dir and arguments.input_dir is not None and len(arguments.input_dir) > 0 and os.path.exists(arguments.input_dir) and os.path.isdir(arguments.input_dir), "Please check your input directory (--input_dir)..."
-    if arguments.input_dir.endswith(os.sep) is False:arguments.input_dir+=os.sep
-    assert arguments.export_dir and arguments.export_dir is not None and len(arguments.export_dir) > 0 , "Please check your export directory (--export_dir)"
-    if arguments.export_dir.endswith(os.sep) is False:arguments.export_dir+=os.sep     
+def morphFaces(input_dir, export_dir, display):
+    assert input_dir and input_dir is not None and len(input_dir) > 0 and os.path.exists(input_dir) and os.path.isdir(input_dir), "Please check your input directory (--input_dir)..."
+    if input_dir.endswith(os.sep) is False:input_dir += os.sep
+    assert export_dir and export_dir is not None and len(export_dir) > 0 , "Please check your export directory (--export_dir)"
+    if export_dir.endswith(os.sep) is False:export_dir += os.sep     
     
-    createExportDir(arguments.export_dir)  
+    createExportDir(export_dir)  
     
-    printProgress(0,2)
-    
+    printProgress(0, 2)    
      
     # Get a list of image paths in a folder
-    imgpaths = facemorpher.list_imgpaths(arguments.input_dir)
+    imgpaths = facemorpher.list_imgpaths(input_dir)
     # To morph, supply an array of face images:
-    facemorpher.morpher(imgpaths,out_video=arguments.export_dir+"morph.avi", plot=boolean_string(arguments.display)) 
+    facemorpher.morpher(imgpaths, out_video=export_dir + "morph.avi", plot=boolean_string(display)) 
     
-    printProgress(1,2)
+    printProgress(1, 2)
      
     # To average, supply an array of face images:
-    facemorpher.averager(collectImageFilePaths(), out_filename=arguments.export_dir+"morph.png",plot=boolean_string(arguments.display)) 
+    facemorpher.averager(collectImageFilePaths(input_dir), out_filename=export_dir + "morph.png", plot=boolean_string(display)) 
      
-    printProgress(2,2)
+    printProgress(2, 2)
     
-    os.system('chmod 777 -R ' + arguments.export_dir)
+    os.system('chmod 777 -R ' + export_dir)
     print("FINISHED...")
+
     
-    
+if __name__ == "__main__":
+    morphFaces(arguments.input_dir, arguments.export_dir, arguments.display)    
